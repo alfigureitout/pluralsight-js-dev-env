3a6355ee74e3e71fa6ca984fd2270b5923fc64f7
Daniel Alfi <danielalfi@gmail.com>
2017-03-30 15:16:57 -0400

    model 9 WIP

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', () => {
+    it('should say Hamilton', (done) => {
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, function(err, window) {
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal("There are so many things I haven't done");
+            done();
+            window.close();
+        });
+    })
+})
